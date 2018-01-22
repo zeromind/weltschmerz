634cc075f1ed754fa3d7350a172d8b4e94f314eb
zeromind <zeromind@gmx.com>
2018-01-22 22:27:06 +0000

    use threaded filehash, better log output

--- filehash.py
+++ filehash.py
@@ -3,6 +3,55 @@
 import hashlib
 import os.path
 import zlib
+from threading import Thread, Semaphore
+
+# Use the chunk size for calculating ed2k as block size
+BLOCKSIZE = 9728000
+
+
+class T(Thread):
+    def __init__(self, h, o):
+        self.h = h
+        self.o = o
+        self.ready = Semaphore(0)
+        self.idle = Semaphore(0)
+        Thread.__init__(self)
+
+    def run(self):
+        while True:
+            self.idle.release()
+            self.ready.acquire()
+            if not len(self.o[0]):
+                return
+            self.h.update(self.o[0])
+
+
+class crc32:
+    def __init__(self):
+        self.v = 0
+
+    def update(self, x):
+        self.v = zlib.crc32(x, self.v)
+
+    def hexdigest(self):
+        return '%08x' % (self.v & 0xffffffff)
+
+
+class ed2k:
+    def __init__(self, file_size):
+        self.v = hashlib.new('md4')
+        self.file_size = file_size
+
+    def update(self, x):
+        if not (self.file_size < BLOCKSIZE):
+            md4data = hashlib.new('md4')
+            md4data.update(x)
+            self.v.update(md4data.digest())
+        else:
+            self.v.update(x)
+
+    def hexdigest(self):
+        return self.v.hexdigest()
 
 
 class FileHash:
@@ -17,34 +66,34 @@ class FileHash:
             Returns file name, size, crc32, md5, sha1, ed2k.
             Uses the red variant of ed2k. Info: http://wiki.anidb.net/w/Ed2k-hash
         '''
-        block_size = 9728000
-        crc32 = 0
-        md5 = hashlib.md5()
-        sha1 = hashlib.sha1()
-        ed2k = hashlib.new('md4')
+        hashes = {'ed2k': ed2k(self.filesize), 'sha1': hashlib.sha1(), 'md5': hashlib.md5(), 'crc32': crc32()}
+        data = [b'']
+        threads = [T(h, data) for n, h in hashes.items()]
+        [t.start() for t in threads]
         with open(self.filename, 'rb') as f:
             while True:
-                data = f.read(block_size)
-                crc32 = zlib.crc32(data, crc32)
-                md5.update(data)
-                sha1.update(data)
-                if not data:
+                data.append(f.read(BLOCKSIZE))
+                [t.idle.acquire() for t in threads]
+                del data[0]
+                [t.ready.release() for t in threads]
+                if not len(data[0]):
                     break
-                if not (self.filesize < block_size):
-                    md4data = hashlib.new('md4')
-                    md4data.update(data)
-                    ed2k.update(md4data.digest())
-                else:
-                    ed2k.update(data)
-        return ("%08x" % (crc32 & 0xffffffff)).lower(), md5.hexdigest(), sha1.hexdigest(), ed2k.hexdigest()
+        [t.join() for t in threads]
+        return ([hashes[h].hexdigest() for h in ['crc32', 'md5', 'sha1', 'ed2k']])
 
 
 if __name__ == '__main__':
     import sys
+    import time
 
     try:
+        start = time.time()
         file_info = FileHash(sys.argv[1])
+        end = time.time()
+        print('%f MB/s' % (file_info.filesize / 1048576. / (end - start)))
         print((file_info.filename, file_info.filesize, file_info.crc32, file_info.md5, file_info.sha1, file_info.ed2k))
+        print('ed2k://|file|{filename}|{filesize}|{ed2k}|/'.format(filename = os.path.basename(file_info.filename).replace("\n",""), filesize=file_info.filesize, ed2k=file_info.ed2k))
+
     except IndexError:
         print('must specify a filename')
     except IOError as err:
--- weltschmerz.py
+++ weltschmerz.py
@@ -11,64 +11,68 @@ import sys
 import threading
 import queue
 import argparse
+import time
 
 config = configparser.ConfigParser()
 config.read_file(open('weltschmerz.cfg'))
 extensions = re.split(' ', config.get('client', 'fileextensions'))
-dbname = config.get('client', 'database', fallback = 'weltschmerz.sqlite3')
+dbname = config.get('client', 'database', fallback='weltschmerz.sqlite3')
 logfile = config.get('client', 'log', fallback='weltschmerz.log')
 folders = re.split(' ', config.get('client', 'folders'))
 num_worker_threads = int(config.get('client', 'threads', fallback=4))
 logging.basicConfig(filename=logfile, format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-#TODO: add absolute path
-def get_files(list):
-        files = []
-        logging.info('Scanning for files...')
-        for path in list:
-                logging.info(''.join(('Scanning for files: ', path)))
-                for dirpath, dirnames, filenames in os.walk(path):
-                        for ext in extensions:
-                                for filename in fnmatch.filter(filenames, ''.join(('*.', ext))):
-                                        if re.search('\udcd7|\udcb7', filename):
-                                                logging.warning(''.join(('skipped a file in ', os.path.abspath(dirpath))))
-                                        else:
-                                                files.append(os.path.join(os.path.abspath(dirpath), filename))
-        logging.info(''.join((str(len(files)), ' files found')))
-        return files
+
+
+# TODO: add absolute path
+def get_files(list, hashed_files):
+    files = []
+    logging.info('Scanning for files...')
+    for path in list:
+        logging.info(''.join(('Scanning for files: ', path)))
+        for dirpath, dirnames, filenames in os.walk(path):
+            for ext in extensions:
+                for filename in fnmatch.filter(filenames, ''.join(('*.', ext))):
+                    if re.search('\udcd7|\udcb7', filename):
+                        logging.warning(''.join(('skipped a file in ', os.path.abspath(dirpath))))
+                    else:
+                        filefullpath = os.path.join(os.path.abspath(dirpath), filename)
+                        if filefullpath not in hashed_files:
+                            files.append(filefullpath)
+                            qin[path].put(filefullpath)
+
+    logging.info(''.join((str(len(files)), ' files found')))
+    return files
+
 
 def db_connect(name):
-        logging.info(''.join(('Establishing database connection (', dbname, ')...')))
-        #if os.path.isfile(name):
-        #        dbc = db.connection()
-        #else:
-        #        dbc = db.connection()
-        #        dbc.initialise_db()
-        dbc = db.connection()
-        dbc.initialise_db()
-        return dbc
+    logging.info(''.join(('Establishing database connection (', dbname, ')...')))
+    # if os.path.isfile(name):
+    #        dbc = db.connection()
+    # else:
+    #        dbc = db.connection()
+    #        dbc.initialise_db()
+    dbc = db.connection()
+    dbc.initialise_db()
+    return dbc
+
 
 def db_disconnect(dbconnector):
-        logging.info(''.join(('Closing database connection (', dbname, ')...')))
-        dbconnector.shutdown()
+    logging.info(''.join(('Closing database connection (', dbname, ')...')))
+    dbconnector.shutdown()
 
-def parse(folders):
-        dbc = db_connect(dbname)
-        files = list(set(get_files(folders)).difference(set(dbc.hashed_files())))
-        db_disconnect(dbc)
-        listlength = len(files)
-        logging.info(''.join((str(listlength), ' files to hash')))
-        for file in files:
-                qin.put(file)
-        return listlength
 
-def parser():
+def parser(folder):
+    print(folder)
     while True:
-        filename = qin.get()
+        filename = qin[folder].get()
+        start = time.time()
         fhash = filehash.FileHash(filename)
+        end = time.time()
         data = (fhash.filename, fhash.filesize, fhash.crc32, fhash.md5, fhash.sha1, fhash.ed2k)
         qout.put(data)
-        logging.info(''.join(('hashed: ', filename)))
-        qin.task_done()
+        logging.info(''.join(('hashed: ', filename, ' %i MB @' % (fhash.filesize / 1048576.), ' %f MB/s' % (fhash.filesize / 1048576. / (end - start)))))
+        qin[folder].task_done()
+
 
 def sql_worker():
     dbc = db_connect(dbname)
@@ -78,19 +82,25 @@ def sql_worker():
         qout.task_done()
     db_disconnect(dbc)
 
+
 if __name__ == "__main__":
-        qin = queue.Queue()
-        qout = queue.Queue()
-        fcount = parse(folders)
-        if fcount > 0:
-            for i in range(num_worker_threads):
-                t = threading.Thread(target=parser)
-                t.daemon = True
-                t.start()
-            sqlt = threading.Thread(target=sql_worker)
-            sqlt.daemon = True
-            sqlt.start()
-            qin.join()
-            qout.join()
-        else:
-            logging.info('Nothing to do: exiting...')
+    dbc = db_connect(dbname)
+    hashed_files = dbc.hashed_files()
+    db_disconnect(dbc)
+    qin = {}
+    for f in folders:
+        qin[f] = queue.Queue()
+
+    qout = queue.Queue()
+    for f in folders:
+        t = threading.Thread(target=parser, kwargs={'folder':f})
+        t.daemon = True
+        t.start()
+    sqlt = threading.Thread(target=sql_worker)
+    sqlt.daemon = True
+    sqlt.start()
+    fcount = get_files(folders, hashed_files)
+    for f,q in qin.items():
+      q.join()
+    qout.join()
+    logging.info('hashed: em all')
