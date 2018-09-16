a01ddc3192918531e07d430200214b9b26e6bfaa
zeromind <zeromind@gmx.com>
2018-09-16 03:37:00 +0000

    adjust variable names

--- test_filehash.py
+++ test_filehash.py
@@ -7,29 +7,29 @@ import filehash
 class TestFilehash(unittest.TestCase):
 
     def test_ed2k_empty(self):
-        empty_file_ed2k = filehash.Ed2k()
-        empty_file_ed2k.update(b'')
-        self.assertEqual(empty_file_ed2k.hexdigest(), '31d6cfe0d16ae931b73c59d7e0c089c0')
+        ed2k_empty = filehash.Ed2k()
+        ed2k_empty.update(b'')
+        self.assertEqual(ed2k_empty.hexdigest(), '31d6cfe0d16ae931b73c59d7e0c089c0')
 
-    def test_ed2k_file_of_zeroes_chunk_sized(self):
-        ed2k_file_of_zeroes_chunk_sized = filehash.Ed2k()
-        ed2k_file_of_zeroes_chunk_sized.update(b'\00' * 9728000)
-        self.assertEqual(ed2k_file_of_zeroes_chunk_sized.hexdigest(), 'fc21d9af828f92a8df64beac3357425d')
+    def test_ed2k_chunk_of_zeroes(self):
+        ed2k_chunk_of_zeroes = filehash.Ed2k()
+        ed2k_chunk_of_zeroes.update(b'\00' * 9728000)
+        self.assertEqual(ed2k_chunk_of_zeroes.hexdigest(), 'fc21d9af828f92a8df64beac3357425d')
 
-    def test_ed2k_file_of_zeroes_double_chunk_sized(self):
-        test_ed2k_file_of_zeroes_double_chunk_sized = filehash.Ed2k()
-        test_ed2k_file_of_zeroes_double_chunk_sized.update(b'\00' * 9728000 * 2)
-        self.assertEqual(test_ed2k_file_of_zeroes_double_chunk_sized.hexdigest(), '114b21c63a74b6ca922291a11177dd5c')
+    def test_ed2k_double_chunk_of_zeroes(self):
+        ed2k_double_chunk_of_zeroes = filehash.Ed2k()
+        ed2k_double_chunk_of_zeroes.update(b'\00' * 9728000 * 2)
+        self.assertEqual(ed2k_double_chunk_of_zeroes.hexdigest(), '114b21c63a74b6ca922291a11177dd5c')
 
     def test_crc32_empty(self):
-        empty_file_crc32 = filehash.Crc32()
-        empty_file_crc32.update(b'')
-        self.assertEqual(empty_file_crc32.hexdigest(), '00000000')
+        crc32_empty = filehash.Crc32()
+        crc32_empty.update(b'')
+        self.assertEqual(crc32_empty.hexdigest(), '00000000')
 
-    def test_crc32_file_of_zeroes_chunk_sized(self):
-        empty_file_crc32 = filehash.Crc32()
-        empty_file_crc32.update(b'\00' * 9728000)
-        self.assertEqual(empty_file_crc32.hexdigest(), '3abc06ba')
+    def test_crc32_chunk_of_zeroes(self):
+        crc32_chunk_of_zeroes = filehash.Crc32()
+        crc32_chunk_of_zeroes.update(b'\00' * 9728000)
+        self.assertEqual(crc32_chunk_of_zeroes.hexdigest(), '3abc06ba')
 
 
 if __name__ == '__main__':
