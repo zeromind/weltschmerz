bd652de11c5b1fa5336ab9bbac306a774e5ab9c7
zeromind <zeromind@gmx.com>
2019-01-17 18:58:52 +0000

    resolve symlinks

--- get_ed2k_links.py
+++ get_ed2k_links.py
@@ -2,10 +2,11 @@
 
 import sys
 import db
+import os
 
 dbc = db.Connection()
 
 directories = sys.argv[1:]
 for directory in directories:
-    for link in dbc.get_ed2k_links(directory):
+    for link in dbc.get_ed2k_links(os.path.realpath(os.path.abspath(directory))):
         print(link)
