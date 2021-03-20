341350e1663a10139027055042d6234427ab4e14
zeromind <zeromind@gmx.com>
2021-03-20 15:49:13 +0000

    more test cases

--- test_filehash.py
+++ test_filehash.py
@@ -7,19 +7,29 @@ import filehash
 class TestFilehash(unittest.TestCase):
 
     def test_ed2k_empty(self):
-        ed2k_empty = filehash.Ed2k()
-        ed2k_empty.update(b'')
-        self.assertEqual(ed2k_empty.hexdigest(), '31d6cfe0d16ae931b73c59d7e0c089c0')
+        ed2k = filehash.Ed2k()
+        ed2k.update(b'')
+        self.assertEqual(ed2k.hexdigest(), '31d6cfe0d16ae931b73c59d7e0c089c0')
 
     def test_ed2k_chunk_of_zeroes(self):
-        ed2k_chunk_of_zeroes = filehash.Ed2k()
-        ed2k_chunk_of_zeroes.update(b'\00' * 9728000)
-        self.assertEqual(ed2k_chunk_of_zeroes.hexdigest(), 'fc21d9af828f92a8df64beac3357425d')
+        ed2k = filehash.Ed2k()
+        ed2k.update(b'\00' * 9728000)
+        self.assertEqual(ed2k.hexdigest(), 'fc21d9af828f92a8df64beac3357425d')
 
     def test_ed2k_double_chunk_of_zeroes(self):
-        ed2k_double_chunk_of_zeroes = filehash.Ed2k()
-        ed2k_double_chunk_of_zeroes.update(b'\00' * 9728000 * 2)
-        self.assertEqual(ed2k_double_chunk_of_zeroes.hexdigest(), '114b21c63a74b6ca922291a11177dd5c')
+        ed2k = filehash.Ed2k()
+        ed2k.update(b'\00' * 9728000 * 2)
+        self.assertEqual(ed2k.hexdigest(), '114b21c63a74b6ca922291a11177dd5c')
+
+    def test_ed2k_lonely_zero(self):
+        ed2k = filehash.Ed2k()
+        ed2k.update(b'\00' * 1)
+        self.assertEqual(ed2k.hexdigest(), '47c61a0fa8738ba77308a8a600f88e4b')
+
+    def test_ed2k_over_nine_thousand_zeroes(self):
+        ed2k = filehash.Ed2k()
+        ed2k.update(b'\00' * 9001)
+        self.assertEqual(ed2k.hexdigest(), '7248d472369fe4e27fe7571b5c4e9a24')
 
     def test_crc32_empty(self):
         crc32_empty = filehash.Crc32()
