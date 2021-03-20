a120b025fb5b7c642e73f57af033a435c9fd0a62
zeromind <zeromind@gmx.com>
2021-03-20 15:48:49 +0000

    update to use sqlalchemy

--- remove_deleted.py
+++ remove_deleted.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
 
-import os
-import db
+import anime
+import os.path
+import logging
 import configparser
-import re
+import argparse
 
-config = configparser.ConfigParser()
-config.read_file(open('weltschmerz.cfg'))
-folders_exclude = re.split(' ', config.get('client', 'folders_exclude'))
 
-dbc = db.Connection()
-hashed_files = dbc.hashed_files()
+def get_config(config_file='weltschmerz.cfg'):
+    config = configparser.ConfigParser()
+    config.read_file(open(config_file))
 
-for directory, filename in hashed_files:
-    for excluded_folder in folders_exclude:
-        if directory.startswith(excluded_folder):
-            print('###### removing excluded {}'.format(os.path.join(directory, filename)))
-            dbc.del_file_by_name(directory, filename)
-            continue
+    parser = argparse.ArgumentParser(
+        description='Hash files and add info to database.')
+    parser.add_argument('--database', help='database to use',
+                        default=config.get('client', 'database'))
+    parser.add_argument('--log-file', dest='log_file', help='logfile to use',
+                        default=config.get('client', 'log'))
+    parser.add_argument('--folder', help='folder to query',
+                        default=None)
+
+    args = parser.parse_args()
+    logging.basicConfig(filename=args.log_file,
+                        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+    return args
+
+
+if __name__ == "__main__":
+    config = get_config()
+    dbs = anime.DatabaseSession(config.database, False)
+    if config.folder:
+        known_files = dbs.session.query(anime.LocalFile).filter(anime.LocalFile.directory.like(f'{config.folder.rstrip("/")}%')).all()
     else:
-        if not os.path.isfile(os.path.join(directory, filename)):
-            print('###### removing {}'.format(os.path.join(directory, filename)))
-            dbc.del_file_by_name(directory, filename)
-dbc.conn.commit()
+        known_files = dbs.session.query(anime.LocalFile).all()
+
+    print(len(known_files))
+    for known_file in known_files:
+        if not os.path.isfile(known_file.full_path()):
+            print(f'###### removing {known_file.full_path()}')
+            dbs.session.delete(known_file)
+        else:
+            print(f'###### found {known_file.full_path()}')
+    dbs.session.commit()
